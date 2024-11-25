834cbe97751449a8a1e794acac3e46bf419d2959
code-web-1 <Rashi337919@gmail.com>
2024-11-25 15:58:15 +0530

    Add files via upload

---
+++ Calculator/TASK 3.js
@@ -0,0 +1,14 @@
+function cleardisplay(){
+    document.getElementById('display').value='';
+}
+function appendToDisplay(value){
+    document.getElementById('display').value +=value;
+}
+function calculateResult(){
+    const display= document.getElementById('display');
+    try {
+        display.value=eval(display.value);
+    } catch (e) {
+        display.value='ERROR';
+    }
+}
